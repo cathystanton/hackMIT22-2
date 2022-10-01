582b07b66af234790aaf3285c58f949f5aed4aee
cathystanton <cjstanton@college.harvard.edu>
2022-10-01 19:14:28 -0400

    commit now

--- react-app-real/src/components/Navbar/index.js
+++ react-app-real/src/components/Navbar/index.js
@@ -10,17 +10,12 @@ const Navbar = ({ props }) => {
       <UpdatedLink to="/">
         <NavLink className={`${(props === "Home") ? "child" : ""}`}>Home</NavLink>
       </UpdatedLink>
-      <UpdatedLink to="/friends">
-        <NavLink className={`${(props === "Friends") ? "child" : ""}`}>About</NavLink>
-      </UpdatedLink><UpdatedLink to="/clubs">
-        <NavLink className={`${(props === "Clubs") ? "child" : ""}`}>Listen</NavLink>
-      </UpdatedLink><UpdatedLink to="/trove">
-        <NavLink className={`${(props === "Trove") ? "child" : ""}`}>Notes</NavLink>
-      </UpdatedLink><UpdatedLink to="/register">
-        <Right className={`${(props === "Register") ? "child" : ""}`}>Report</Right>
-      </UpdatedLink><UpdatedLink to="/login">
-        <Right className={`${(props === "Login") ? "child" : ""}`}>Sign Up</Right>
-      </UpdatedLink>
+      <UpdatedLink to="/about">
+        <NavLink className={`${(props === "About") ? "child" : ""}`}>About</NavLink>
+      </UpdatedLink><UpdatedLink to="/listen">
+        <NavLink className={`${(props === "Listen") ? "child" : ""}`}>Listen</NavLink>
+      </UpdatedLink><UpdatedLink to="/report">
+        <Right className={`${(props === "Report") ? "child" : ""}`}>Report</Right>
       </UpdatedLink><UpdatedLink to="/login">
         <Right className={`${(props === "Login") ? "child" : ""}`}>Login</Right>
       </UpdatedLink>
--- react-app-real/src/components/Navbar/styles.js
+++ react-app-real/src/components/Navbar/styles.js
@@ -2,34 +2,29 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const Rectangle = styled.div`
-  background-color: ${({theme}) => theme.navbar};
+  background-color: ${({theme}) => theme.color.layout.navbar};
   width: 100vw;
   height: 10vh;
 `
 
 export const NavLink = styled.div`
-  font-family: ${({theme}) => theme.font.paragraph_font};
+  font-family: ${({theme}) => theme.font.body.font_family};
   font-size: 16px;
   display: inline-block;
   height: 10vh;
   padding-right: 5vh;
   padding-left: 5vh;
   line-height: 10vh;
-  color: ${({theme}) => theme.font.paragraph_color};
-
-  &:hover {
-    background-color: ${({theme}) => theme.hover};
-    color: ${({theme}) => theme.site_background};
-  };
+  color: ${({theme}) => theme.color.font.paragraph};
 
   &.child {
-    background-color: ${({theme}) => theme.on_page};
-    color: ${({theme}) => theme.site_background};
+    background-color: ${({theme}) => theme.color.layout.navbar_highlight};
+    color: ${({theme}) => theme.color.layout.background};
   };
 `
 
 export const Right = styled.div`
-  font-family: ${({theme}) => theme.font.paragraph_font};
+  font-family: ${({theme}) => theme.font.body.font_family};
   font-size: 16px;
   display: inline-flexbox;
   float: right;
@@ -38,22 +33,17 @@ export const Right = styled.div`
   padding-left: 5vh;
   line-height: 10vh;
 
-  &:hover {
-    background-color: ${({theme}) => theme.hover};
-    color: ${({theme}) => theme.site_background};
-  };
-
   &.true {
-    background-color: ${({theme}) => theme.on_page};
-    color: ${({theme}) => theme.site_background};
+    background-color: ${({theme}) => theme.color.layout.navbar_highlight};
+    color: ${({theme}) => theme.color.layout.background};
   };
 `
 
 export const UpdatedLink = styled(Link)`
-  color: ${({theme}) => theme.font.paragraph_color};
+  color: ${({theme}) => theme.color.layout.};
   text-decoration: none;
 
   &:hover {
-    color: ${({theme}) => theme.site_background};
+    color: ${({theme}) => theme.color.font.paragraph};
   }
 `
