ce500566cd1e67c5156cbc0447f0dfd879efe561
wilkyrlx <john_wilkinson@brown.edu>
2022-10-01 21:57:17 -0400

    baseline backend functionality - DB updates

--- react-app-real/backend/storeData.py
+++ react-app-real/backend/storeData.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Request
+from flask import Flask, render_template, Request, redirect
 from markupsafe import escape
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
@@ -7,37 +7,40 @@ import hashlib
 
 app = Flask(__name__)
 db_path = 'react-app-real/backend/mock.db'
-connection = sqlite3.connect(db_path)
-
-def get_db_connection():
-    conn = sqlite3.connect(db_path)
-    conn.row_factory = sqlite3.Row
-    return conn
 
 # anonymizes username into unique hash
 def processUsername(username):
     m = hashlib.md5()
-    m.update(username)
+    m.update(username.encode("UTF-8"))
     return str(int(m.hexdigest(), 16))[0:12]
-
-
 
 # index page. Should filter for user
 @app.route("/")
 def index():
-    conn = get_db_connection()
+    conn = sqlite3.connect(db_path, isolation_level=None)
+    conn.row_factory = sqlite3.Row
     posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('index.html', posts=posts)
 
-# adds a post to the database. User is hardcoded as 1, add later when we get 
-# multi-user functionality - is this the right app.route approach?
+# adds entry to DB, refreshes page
 @app.route('/<username>/<songID>/<journalEntry>/<emotion>')
 def add_post(username, songID, journalEntry, emotion):
-    with open('react-app-real/backend/schema.sql') as f:
-        connection.executescript(f.read())
+    connection = sqlite3.connect(db_path, isolation_level=None)
     cur = connection.cursor()
     cur.execute("INSERT INTO posts (userID, songID, journalEntry, emotion) VALUES (?, ?, ?, ?)",
             (processUsername(username), songID, journalEntry, emotion))
     connection.commit()
     connection.close()
+
+    # refreshes page
+    return redirect('/')
+
+
+# DEV only - initializes SQL if it is empty
+def new_SQL():
+    connection = sqlite3.connect(db_path, isolation_level=None)
+    connection.row_factory = sqlite3.Row
+    with open('react-app-real/backend/schema.sql') as f:
+        connection.executescript(f.read())
+
